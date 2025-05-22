51fed0c6c81b43735b5349b3a139b4752422abfe
Caio Luan <157083928+Takakae09@users.noreply.github.com>
2025-05-22 17:47:23 -0300

    Add files via upload

---
+++ javascript/scripts/script.js
@@ -0,0 +1,23 @@
+
+function converter (){
+    let n1 =parseInt(document.querySelector("#valor").value)
+    let operation = document.querySelector("select").value
+    let resultado = document.querySelector("#resultado")
+    let calculo = 0
+    
+   switch(operation){
+    case'dolar':
+    calculo = n1*5.668
+    resultado.innerHTML = calculo
+    break
+    case'euro':
+    calculo = n1*6.2
+    resultado.innerHTML = calculo
+    break
+    case'bitcoin':
+    calculo = n1*0.0000032
+    resultado.innerHTML = calculo
+    break
+   }
+     
+}
