329f08868306bd04516b72db11b109394951c2a4
Caio Luan <157083928+Takakae09@users.noreply.github.com>
2026-04-17 14:21:59 -0300

    mod 1

--- javascript/scripts/script.js
+++ javascript/scripts/script.js
@@ -1,4 +1,5 @@
 
+
 function converter (){
     let n1 =parseInt(document.querySelector("#valor").value)
     let operation = document.querySelector("select").value
@@ -19,5 +20,6 @@ function converter (){
     resultado.innerHTML = calculo
     break
    }
+
      
 }
